f8a7b3df0cefec63e7d1c04a20e1e1de478394c4
LukeHxH <lucasmed812@gmail.com>
2019-02-01 12:34:32 -0300

    mostra intensidade (fixa) na tela

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 const firePixelsArray = [];
-const fireWidth = 2;
-const fireHeight = 3;
+const fireWidth = 10;
+const fireHeight = 10;
 
 function init() {
     createFireDataStructure();
 
-    console.log(firePixelsArray);
+    render();
 }
 
 function createFireDataStructure() {
     const numberOfPixels = fireWidth * fireHeight;
 
     for (let i = 0; i < numberOfPixels; i++) {
-        firePixelsArray[i] = 0;
+        firePixelsArray[i] = 1;
     }
 }
 
-function calculatePropagation() {
-
-}
-
 function render() {
+    let tableHtml = '<table cellspacing=0 cellpadding=0>';
 
+    for (let row = 0; row < fireHeight; row++) {
+        tableHtml += '<tr>';
+
+        for (let column = 0; column < fireWidth; column++) {
+            const pixelIndex = column + (fireWidth * row);
+            const fireIntensity = firePixelsArray[pixelIndex];
+
+            tableHtml += '<td>';
+            tableHtml += `<div class="pixel-index">${pixelIndex}</div>`;
+            tableHtml += `<div class="fire-intensity">${fireIntensity}</div>`
+            tableHtml += '</td>';
+        }
+
+        tableHtml += '</tr>';
+    }
+
+    tableHtml += '</table>'
+
+    let fireCanvasWidget = document.querySelector("#fire-canvas");
+    fireCanvasWidget.innerHTML = tableHtml;
 }
 
 init();
